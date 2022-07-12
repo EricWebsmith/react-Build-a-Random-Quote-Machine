0e40d058fb1142154d45ee3a58c35a095223b3f1
Eric Websmith <eric.websmith@gmail.com>
2022-07-12 15:06:55 +0800

    delete build

--- src/Quote.js
+++ src/Quote.js
@@ -15,12 +15,12 @@ const Quote = () => {
 
     //const name = getName();
     return (
-        <div className="Quote">
+        <div className="Quote" id="quote-box">
             <p id="text">{quote.text}</p>
             <p id="author">{quote.author}</p>
             <div className="btnContainer" >
-            <button className="btn" onClick={getQuotes}>Get Quote</button>
-            <a
+            <button id="new-quote" className="btn" onClick={getQuotes}>Get Quote</button>
+            <a id="tweet-quote"
                 href={`https://twitter.com/intent/tweet?text=${quote.text}`}
                 target="_blank"
                 rel="noopener noreferrer"
